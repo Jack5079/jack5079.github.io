36ab8081c78e0e1df529d52d3268501d858fc7ef
Jack5079 <29169102+Jack5079@users.noreply.github.com>
2018-10-11 20:33:31 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-var channelID = "UCC1luu48wngHte-glhyvMHQ";
-$.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'+channelID, function(data) {
-   var link = data.items[0].link;
-   var id = link.substr(link.indexOf("=")+1);
-    $("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
-});
+
 
 (function($) {
 
