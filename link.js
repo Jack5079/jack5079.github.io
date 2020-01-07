1ea44b81c4bf2757b39b0e4be575284c0737f53b
Jack <29169102+Jack5079@users.noreply.github.com>
2020-01-07 18:47:40 -0500

    thingy

--- link.js
+++ link.js
@@ -1,4 +1,17 @@
 /* global location */
+const style = document.createElement('style')
+style.innerHTML = `
+  iframe {
+    display: none;
+    position: fixed;
+    border: 0;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+`
+document.head.appendChild(style)
 function onclick (e) {
   if (!this.href.startsWith('#')) {
     e.preventDefault()
@@ -7,27 +20,29 @@ function onclick (e) {
     document.body.appendChild(frame)
     frame.onload = _ => {
       frame.style.display = 'block'
-      frame.animate([
-        // keyframes
-        { top: '100%', opacity: 0 },
-        { top: 0, opacity: 1 }
-      ], {
-        // timing options
-        duration: 1000,
-        easing: 'ease-out',
-        iterations: 1
-      }).onfinish = _ => { location.href = this.getAttribute('href') }
+      frame.animate(
+        [
+          // keyframes
+          { top: '100%', opacity: 0 },
+          { top: 0, opacity: 1 }
+        ],
+        {
+          // timing options
+          duration: 1000,
+          easing: 'ease-out',
+          iterations: 1
+        }
+      ).onfinish = _ => {
+        location.href = this.getAttribute('href')
+      }
     }
-    frame.style.display = 'none'
-    frame.style.border = '0'
-    frame.style.background = 'white'
-    frame.style.width = '100vw'
-    frame.style.height = '100vh'
-    frame.style.position = 'fixed'
-    frame.style.top = '0'
-    frame.style.left = '0'
-    document.querySelectorAll('*').forEach(e => { e.style.pointerEvents = 'none' })
+
+    document.querySelectorAll('*').forEach(e => {
+      e.style.pointerEvents = 'none'
+    })
   }
 }
 
-document.querySelectorAll('a').forEach(e => e.addEventListener('click', onclick))
+document
+  .querySelectorAll('a')
+  .forEach(e => e.addEventListener('click', onclick))
