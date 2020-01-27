996e8ee8298a6adf16393b9d530a94d7a6ce3a59
Jack <29169102+Jack5079@users.noreply.github.com>
2020-01-26 19:22:14 -0500

    fetus deletus

--- redirect.js
+++
@@ -1,5 +0,0 @@
-/* global location */
-const locationoflink = [...document.getElementsByTagName('a')].map(e => e.innerText).indexOf(location.hash.substring(1))
-if (locationoflink !== -1) {
-  [...document.getElementsByTagName('a')][locationoflink].click()
-}
